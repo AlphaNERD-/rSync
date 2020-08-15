af3eb81f84a04baf7ec651283568442e006416de
Julia Wolf <lovelydeath94@googlemail.com>
2020-08-15 17:26:05 +0200

    First commit

---
+++ rSync.Core/Classes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rSync.Core
+{
+    public class GameCollection
+    {
+        public Platforms Platform;
+        public string PlatformName;
+        public string GamePath;
+        public string SavePath;
+    }
+
+    public class Console
+    {
+        public string Name;
+        public string RootPath;
+        public List<GameCollection> Collections;
+        public ITransferInterface FileTransfer;
+    }
+
+    public enum Platforms
+    {
+        Unknown = 0,
+        NDS,
+        ThreeDS,
+        GBA,
+        PSP,
+        GB,
+        GBC
+    }
+
+    public class SaveFile
+    {
+        public string SaveFileName;
+        public DateTime FileChanged;
+    }
+
+    public interface ITransferInterface
+    {
+        public virtual SaveFile[] GetSaveFileList() { return new SaveFile[0]; }
+
+        public virtual void GetSaveFile(SaveFile sf) { }
+
+        public virtual void PutSaveFile(SaveFile sf) { }
+    }
+}
---
+++ rSync.Core/Runner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rSync.Core
+{
+    public class Runner
+    {
+        public List<GameCollection> MasterCollection;
+        public List<Console> ConnectedConsoles;
+    }
+}
